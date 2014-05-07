b2611b152ae55635013f452786b549a3a81deb4f
piwi91 <pimwiddershoven@gmail.com>
2014-05-07 13:52:11 +0200

    Ignore PHP warnings for fsockopen
    Because we already check if the socket isn't false, you should ignore PHP warnings. In the CGI/FPM PHP errors aren't displayed. In CLI mode it isn't.

--- src/SupervisorClient/SupervisorClient.php
+++ src/SupervisorClient/SupervisorClient.php
@@ -242,7 +242,7 @@ class SupervisorClient
         // Open socket if needed.
 
         if (is_null($this->_socket)) {
-            $this->_socket = fsockopen($this->_hostname, $this->_port, $errno, $errstr, $this->_timeout);
+            $this->_socket = @fsockopen($this->_hostname, $this->_port, $errno, $errstr, $this->_timeout);
 
             if (!$this->_socket) {
                 throw new Exception(sprintf("Cannot open socket: Error %d: \"%s\"", $errno, $errstr));
